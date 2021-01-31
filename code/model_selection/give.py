ba19588e0f84f517534b671e1d91351862ae3f3c
Fathy Shalaby <47745314+fathyshalaby@users.noreply.github.com>
2021-01-31 09:48:05 +0100

    Delete give.py

--- code/model_selection/give.py
+++
@@ -1,6 +0,0 @@
-import pickle
-with open('/../preprocessing/goid_human.pkl', 'rb') as f:
-    # Pickle will store our object into the specified file
-    ggid = pickle.load(f)
-
-print(ggid.values())
